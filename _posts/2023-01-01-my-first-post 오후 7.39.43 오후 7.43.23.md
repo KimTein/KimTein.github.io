89a0abc4278316d40ac999fa4ba46172e461d4ee
KimTein <jegarkong@gmail.com>
2023-01-03 23:21:13 +0900

    test

---
+++ _posts/2023-01-01-my-first-post 오후 7.39.43 오후 7.43.23.md
@@ -0,0 +1,15 @@
+---
+title: "[Daily]My first post!"
+#permalink: /categories/
+author: KimTein
+categories: 
+    - daily
+tags:
+    - new blog
+    - first post
+last_modified_at: 2023-01-01 02:44:30 +0900
+---
+
+2023년이 되었습니다. 새로운 마음으로 블로그를 작성해나가 보도록 하겠습니다. 
+
+인공지능과 물리를 다루어보며 정리해보도록 할게요. 모두 새해 복 많이 받으시길 바랍니다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "[Daily]My first post!",
+        "excerpt":"2023년이 되었습니다. 새로운 마음으로 블로그를 작성해나가 보도록 하겠습니다.   인공지능과 물리를 다루어보며 정리해보도록 할게요. 모두 새해 복 많이 받으시길 바랍니다.  ","categories": ["daily"],
+        "tags": ["new blog","first post"],
+        "url": "/daily/my-first-post-%EC%98%A4%ED%9B%84-7.39.43-%EC%98%A4%ED%9B%84-7.43.23/",
+        "teaser": null
+      },{
+        "title": "[Daily]customizing my blog!",
+        "excerpt":"  블로그를 꾸며보는데 있어서 마크다운을 이용하여 작성한다는 점이 어렵네요.   천천히 채워나가보도록 하겠습니다.   public class BootSpringBootApplication {   public static void main(String[] args) {     System.out.println(\"Hello, Honeymon\");   } }     ","categories": ["daily"],
+        "tags": ["customizing","test"],
+        "url": "/daily/post-%EC%98%A4%ED%9B%84-7.43.23/",
+        "teaser": null
+      }]
